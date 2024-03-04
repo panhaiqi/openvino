913f9e65635e6a9f81131fc77a32c7d8ae11a34d
panhaiqi <haiqi.pan@intel.com>
2024-03-04 16:00:34 +0800

    port frameworks.ai.openvino.tests to this pr

--- tests/samples_tests/smoke_tests/test_benchmark_app.py
+++ tests/samples_tests/smoke_tests/test_benchmark_app.py
@@ -60,7 +60,38 @@ class TestBenchmarkApp(SamplesCommonTestClass):
     def test_benchmark_app_fp32_sync(self, param):
         _check_output(self, param)
 
+def stream_checker(param, config_json):
+    device = param.get('d', 'CPU')
+    if device not in config_json.keys():
+        log.error("device not found")
+        return False
+    if 'NUM_STREAMS' not in config_json[device].keys():
+        log.error("NUM_STREAMS not found")
+        return False
+    if param['nstreams'] == config_json[device]['NUM_STREAMS']:
+        return True
+    else:
+        log.error("value of nstreams is false")
+        return False
 
+def pin_checker(param, config_json):
+    device = param.get('d', 'CPU')
+    if device not in config_json.keys():
+        log.error("device not found")
+        return False
+    if 'AFFINITY' not in config_json[device].keys():
+        log.error("AFFINITY not found")
+        return False
+    if param['pin'] == 'YES' and config_json[device]['AFFINITY'] == 'CORE':
+        return True
+    elif param['pin'] == 'NO' and config_json[device]['AFFINITY'] == 'NONE':
+        return True
+    elif param['pin'] == config_json[device]['AFFINITY']:
+        return True
+    else:
+        log.error("value of pin is false")
+        return False
+    
 def _check_output(self, param):
     """
     Benchmark_app has functional and accuracy testing.
@@ -68,7 +99,22 @@ def _check_output(self, param):
     """
 
     # Run _test function, that returns stdout or 0.
+    # 0 is for device FPGA, Myriad, HDDL - skip accuraccy check on these devices
+    if 'dump_config' not in param:
+        param['dump_config'] = os.path.join(os.environ.get('WORKSPACE'), 'config.json')
     stdout = self._test(param)
+    print(stdout)
+    config_file_name = param['dump_config']
+    config = open(config_file_name)
+    lines = config.readlines()
+    print('config file name:', param['dump_config'])
+    for line in lines:
+        print(line)
+    config.seek(0, 0)
+    if not config:
+        return 0
+    config_json = json.load(config) 
+    config.close()
     if not stdout:
         return 0
     stdout = stdout.split('\n')
@@ -80,7 +126,9 @@ def _check_output(self, param):
     if is_ok == False:
         flag = 1
         log.error("No FPS in output")
+    assert flag == 0, "Wrong output of FPS"
 
+    flag = 0
     is_ok = False
     for line in stdout:
         if 'Latency' in line:
@@ -88,5 +136,19 @@ def _check_output(self, param):
     if is_ok == False:
         flag = 1
         log.error("No Latency in output")
-    assert flag == 0, "Wrong output of this sample"
+    assert flag == 0, "Wrong output of Latency"
+
+    is_ok = False
+    if 'nstreams' in param:
+        is_ok = stream_checker(param, config_json)
+        if is_ok == False:
+            log.error("No expected nstreams in output")
+            assert False, "check nstreams failed"
+    is_ok = False
+    if 'pin' in param:
+        is_ok = pin_checker(param, config_json)
+        if is_ok == False:
+            log.error("No expected pin in output")
+            assert False, "check pin failed"
+
     log.info('Accuracy passed')
